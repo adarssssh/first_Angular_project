560ee20c10c44cf30876f67aaff73d9f2c87bcde
adarssssh <adarsht06@gmail.com>
2024-09-18 23:37:52 +0530

    Initial commit

---
+++ src/app/components/designation/designation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-designation',
+  standalone: true,
+  imports: [],
+  templateUrl: './designation.component.html',
+  styleUrl: './designation.component.css'
+})
+export class DesignationComponent {
+
+}
